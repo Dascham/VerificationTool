34ec2753e9c6e83b9b2c737c84b03a7bde91037e
Xiazee <Xiazee@users.noreply.github.com>
2019-04-16 12:31:45 +0200

    Added types for State, Model, Process, etc.

---
+++ cpp/src/State.h
@@ -0,0 +1,16 @@
+#ifndef CPP_STATE_H
+#define CPP_STATE_H
+
+
+#include <vector>
+
+struct State {
+    std::vector<size_t> locations;
+    std::vector<int> variables;
+    // Clocks/Zones/DBM go here
+
+
+};
+
+
+#endif //CPP_STATE_H
