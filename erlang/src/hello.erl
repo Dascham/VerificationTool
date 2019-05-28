5feb99ebb9390fdbeab33ec5af2d38007816278a
misterpaws <mchris09@student.aau.dk>
2019-05-28 04:24:16 +0200

    i am not a clever man

--- erlang/src/hello.erl
+++ erlang/src/hello.erl
@@ -1,4 +1,25 @@
--module(hello).
--export([hello_world/0]).
+-module(modelChecker).
 
-hello_world() -> io:fwrite("hello, world\n").
+-export([startMC/0, createProcess/0, explore/1, createNode/0, createEdge/0]).
+
+%setup model, create processes and start exploring
+
+startMC() -> 0.
+
+%create process from nodes and edges
+
+createProcess() -> 0.
+
+%create node from name and invariants
+
+createNode() -> 0.
+
+%create edge from starting node, target node, guards and updates
+
+createEdge() -> 0.
+
+%returns all state vectors reachable in one step from the input vector
+
+explore(stateVector) -> io:fwrite("hello, world\n").
+
+
